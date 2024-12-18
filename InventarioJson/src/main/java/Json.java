5699bce1a51f50f33ec5f0218edb22704ebe69c9
Jose <mortox1990@gmail.com>
2024-12-18 19:08:22 +0100

    empezando Json

--- InventarioJson/src/main/java/Json.java
+++ InventarioJson/src/main/java/Json.java
@@ -1,2 +1,12 @@
+import org.json.simple.parser.JSONParser;
+
 public class Json {
+
+    public static void leerJson() throws org.json.simple.parser.ParseException{
+        JSONParser parser = new JSONParser();
+
+    }
+
+
+
 }
