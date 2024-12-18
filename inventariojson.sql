e2ba2a632bacd2add7b732129ea48da2980ccc46
Jose <56450598+NeonArchon@users.noreply.github.com>
2024-12-18 18:27:59 +0100

    SQL
    el script SQL para el programa (una segunda copia será añadida en este)

---
+++ inventariojson.sql
@@ -0,0 +1,15 @@
+-- DROP DATABASE IF NOT EXISTS inventariojson;
+
+CREATE DATABASE inventariojson;
+
+SELECT inventariojson;
+
+CREATE TABLE productos (
+	codigo VARCHAR(20) PRIMARY KEY,
+    categoria VARCHAR(20) NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    precio INT NOT NULL,
+    fecha_ingreso DATE NOT NULL,
+    fstock INT NOT NULL
+);
+
