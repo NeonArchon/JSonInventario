32931a85b7dd8538f5b9a8083d57252658aadee7
morto <mortox1990@gmail.com>
2025-01-05 13:53:16 +0100

    Limpiando un poco el codigo

--- InventarioJson/src/main/java/org/example/Json.java
+++ InventarioJson/src/main/java/org/example/Json.java
@@ -12,7 +12,7 @@ public class Json {
 
     public static void leerJson() throws org.json.simple.parser.ParseException {
         JSONParser parser = new JSONParser();
-        try (FileReader reader = new FileReader("C:\\Users\\alumno\\Desktop\\JSonInventario\\InventarioJson\\src\\main\\java\\org\\example\\inventario.json")) {
+        try (FileReader reader = new FileReader("C:\\Users\\morto\\IdeaProjects\\JSonInventario\\InventarioJson\\src\\main\\java\\org\\example\\inventario.json")) {
             Object obj = parser.parse(reader);
             JSONObject arrayiventario = (JSONObject) obj;
 
@@ -31,48 +31,20 @@ public class Json {
             System.out.println("**************");
             JSONObject jsinventario = (JSONObject) jsonobject.get("inventario");
             JSONObject jsproductos = (JSONObject) jsinventario.get("productos");
-
-            /*
-            String inv = (String) jsproducto.get("productos");
-            System.out.println("*******Productos: " + inv);
-*/
-/*
-            JSONObject jsproductos = (JSONObject) jsonobject.get("productos");
-            String prod = (String) jsproductos.get("productos");
-            System.out.println("Productos " + prod);*/
-
-
             JSONArray japroducto = (JSONArray) jsproductos.get("producto");
             for (Object pr : japroducto){
 
                 JSONObject p = (JSONObject) pr;
 
-                /*JSONObject jscodigo = (JSONObject)
-                String cod = (String) jscodigo.get("-codigo");*/
                 System.out.println("Codigo: " + p.get("-codigo"));
-
-            //    JSONObject jscategoria = (JSONObject) jsonobject.get("-categoria");
-             //   String car = (String) jscategoria.get("-categoria");
 
                 System.out.println("Categoria " + p.get("-categoria"));
 
-             //   JSONObject jsnombre = (JSONObject) jsonobject.get("nombre");
-              //  String nom = (String) jsnombre.get("nombre");
-
                 System.out.println("Nombre " + p.get("nombre"));
-
-              //  JSONObject jsprecio = (JSONObject) jsonobject.get("precio");
-               // String pre = (String) jsprecio.get("precio");
 
                 System.out.println("Precio" + p.get("precio"));
 
-         //       JSONObject jsfecha = (JSONObject) jsonobject.get("fecha_ingreso");
-              //  String fecha = (String) jsfecha.get("fecha_ingreso");
-
                 System.out.println("Fecha de Ingreso " + p.get("fecha_ingreso"));
-
-              //  JSONObject jsstock = (JSONObject) jsonobject.get("stock");
-               // String stock = (String) jsstock.get("stock");
 
                 System.out.println("stock " + p.get("stock"));
 
